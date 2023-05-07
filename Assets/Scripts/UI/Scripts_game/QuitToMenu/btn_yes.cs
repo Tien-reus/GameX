7c89e84572fce7a2cbee3d300aec218f14349f8e
tien <tientong2802@gmail.com>
2023-05-07 19:57:50 +0700

    first commit

---
+++ Assets/Scripts/UI/Scripts_game/QuitToMenu/btn_yes.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class btn_yes : MonoBehaviour
+{
+    public void yes()
+    {
+        Destroy(GameObject.);
+        SceneManager.LoadScene(0);
+    }
+}
